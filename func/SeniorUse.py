82f576de4aedf24dcceae54dcf84f2ace9f6d745
kuangkai_jy@163.com <kuangkai_jy@163.com>
2018-11-13 22:06:12 +0800

    增加偏函数和装饰器的例子

--- func/SeniorUse.py
+++ func/SeniorUse.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from collections import Iterable
-from collections import Iterator
+from collections.abc import Iterable
+from collections.abc import Iterator
+import functools
 
 #切片
 L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
@@ -145,3 +146,79 @@ print(isinstance('abc', Iterator))
 只有在需要返回下一个数据时它才会计算。
 Iterator甚至可以表示一个无限大的数据流，例如全体自然数。而使用list是永远不可能存储全体自然数的。
 '''
+
+#匿名函数,类似于java里面的lambda表达式
+print(list(map(lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
+
+f = lambda x: x * x
+print(f)
+# output: <function <lambda> at 0x7f38c778a7b8>
+print(f(5))
+
+#装饰器
+
+#函数对象有一个__name__属性，可以拿到函数的名字：
+def now():
+    print('now time is 17')
+
+now()
+print(now.__name__)
+
+#现在，假设我们要增强now()函数的功能，比如，在函数调用前后自动打印日志，但又不希望修改now()函数的定义，这种在代码运行期间动态增加功能的方式，称之为“装饰器”（Decorator）。
+#本质上，decorator就是一个返回函数的高阶函数。所以，我们要定义一个能打印日志的decorator，可以定义如下：
+def log(func):
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+print('======================')
+#观察上面的log，因为它是一个decorator，所以接受一个函数作为参数，并返回一个函数。我们要借助Python的@语法，把decorator置于函数的定义处：
+@log
+def now():
+    print('now time is 17')
+now()
+
+#如果decorator本身需要传入参数，那就需要编写一个返回decorator的高阶函数，写出来会更复杂。比如，要自定义log的文本：
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+@log('execute')
+def now():
+    print('2015-3-25')
+
+now()
+print(now.__name__)#wrapper
+
+#一个完整的decorator的写法如下：
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+#或者针对带参数的decorator：
+def log(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+#偏函数
+#int()函数还提供额外的base参数，默认值为10。如果传入base参数，就可以做N进制的转换：
+print(int('12345',base=8))
+
+#functools.partial就是帮助我们创建一个偏函数的，不需要我们自己定义int2()，可以直接使用下面的代码创建一个新的函数int2：
+int2 = functools.partial(int, base=2)
+print(int2('1000000'))
+
+#所以，简单总结functools.partial的作用就是，把一个函数的某些参数给固定住（也就是设置默认值），返回一个新的函数，调用这个新函数会更简单
