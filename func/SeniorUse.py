3004cc370772d6a35b8612a917d3202315f25d31
kuangkai <kuangkai_jy@163.com>
2018-11-13 19:39:31 +0800

    增加列表 迭代器等一些例子

--- func/SeniorUse.py
+++ func/SeniorUse.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from collections import Iterable
+from collections import Iterator
 
 #切片
 L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
@@ -35,4 +36,112 @@ for x, y in [(1, 1), (2, 4), (3, 9)]:
 #举个例子，要生成list [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]可以用list(range(1, 11))：
 print(list(range(1,11)))
 
+#写列表生成式时，把要生成的元素x * x放到前面，后面跟for循环，就可以把list创建出来，十分有用，多写几次，很快就可以熟悉这种语法。
 print([x * x for x in range(1, 11)])
+
+#for循环后面还可以加上if判断，这样我们就可以筛选出仅偶数的平方：
+print([x * x for x in range(1, 11) if x % 2 == 0])
+l = []
+[l.append(x) for x in range(1,11) if x % 2 == 0]
+print(l)
+
+#还可以使用两层循环，可以生成全排列：
+print([m + n for m in 'ABC' for n in 'XYZ'])
+
+#因此，列表生成式也可以使用两个变量来生成list：
+d = {'x': 'A', 'y': 'B', 'z': 'C' }
+print([k + '=' + v for k, v in d.items()])
+
+#最后把一个list中所有的字符串变成小写：
+L = ['Hello', 'World', 'IBM', 'Apple']
+print([s.lower() for s in L])
+
+#生成器
+#在Python中，这种一边循环一边计算的机制，称为生成器
+#要创建一个generator，有很多种方法。第一种方法很简单，只要把一个列表生成式的[]改成()，就创建了一个generator：
+L = [x * x for x in range(10)]
+print(L)
+g = (x * x for x in range(10))
+#如果要一个一个打印出来，可以通过next()函数获得generator的下一个返回值：
+print(next(g))
+
+#通过循环来取生成器中的值,建议使用这种方法
+g = (x * x for x in range(10))
+for n in g:
+    print(n)
+
+# 斐波拉契数列用列表生成式写不出来，但是，用函数把它打印出来却很容易：
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+print(fib(6))
+'''
+仔细观察，可以看出，fib函数实际上是定义了斐波拉契数列的推算规则，可以从第一个元素开始，推算出后续任意的元素，这种逻辑其实非常类似generator。
+也就是说，上面的函数和generator仅一步之遥。要把fib函数变成generator，只需要把print(b)改为yield b就可以了：
+'''
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+f = fib(6)
+print(f)
+
+'''
+这里，最难理解的就是generator和函数的执行流程不一样。函数是顺序执行，遇到return语句或者最后一行函数语句就返回。而变成generator的函数，
+在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行。
+举个简单的例子，定义一个generator，依次返回数字1，3，5：
+'''
+def odd():
+    print('step 1')
+    yield 1
+    print('step 2')
+    yield(3)
+    print('step 3')
+    yield(5)
+
+o = odd()
+print(next(o))
+print(next(o))
+print(next(o))
+
+
+g = fib(6)
+
+while True:
+    try:
+        x = next(g)
+        print('g:',x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
+
+#迭代器
+#可以使用isinstance()判断一个对象是否是Iterable对象：
+print(isinstance([], Iterable))
+print(isinstance((), Iterable))
+print(isinstance('abc', Iterable))
+print(isinstance((x for x in range(10)), Iterable))
+print(isinstance(100, Iterable))
+
+
+#可以使用isinstance()判断一个对象是否是Iterator对象：
+print(isinstance((x for x in range(10)), Iterator))
+print(isinstance([], Iterator))
+print(isinstance({}, Iterator))
+print(isinstance('abc', Iterator))
+
+#生成器都是Iterator对象，但list、dict、str虽然是Iterable，却不是Iterator。
+'''
+这是因为Python的Iterator对象表示的是一个数据流，Iterator对象可以被next()函数调用并不断返回下一个数据，直到没有数据时抛出StopIteration错误。
+可以把这个数据流看做是一个有序序列，但我们却不能提前知道序列的长度，只能不断通过next()函数实现按需计算下一个数据，所以Iterator的计算是惰性的，
+只有在需要返回下一个数据时它才会计算。
+Iterator甚至可以表示一个无限大的数据流，例如全体自然数。而使用list是永远不可能存储全体自然数的。
+'''
