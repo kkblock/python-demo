d0c13b6098404510d0e4b25dbfea99d45cc86d47
kuangkai <kuangkai_jy@163.com>
2018-11-14 18:51:50 +0800

    增加面向对象高级程序设计示例代码

--- oop/meta.py
+++ oop/meta.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 class Hi(object):
-    def fn(self, name='world'):  # 先定义函数
-        print('Hello, %s.' % name)
+    pass
 
 
 '''
@@ -16,4 +15,26 @@ h = Hi()
 print(type(h))
 
 
-#这里暂时省略使用方式
+def fn(self, name='world'):  # 先定义函数
+    print('Hello, %s.' % name)
+    return 'abc'
+
+
+Hello = type('Hi', (object,), dict(hello=fn))  # 创建Hello class
+h = Hello()
+print(h.hello())
+print(type(h))
+
+'''
+要创建一个class对象，type()函数依次传入3个参数：
+
+1.class的名称；
+2.继承的父类集合，注意Python支持多重继承，如果只有一个父类，别忘了tuple的单元素写法；
+3.class的方法名称与函数绑定，这里我们把函数fn绑定到方法名hello上。
+
+通过type()函数创建的类和直接写class是完全一样的，因为Python解释器遇到class定义时，仅仅是扫描一下class定义的语法，然后调用type()函数创建出class。
+正常情况下，我们都用class Xxx...来定义类，但是，type()函数也允许我们动态创建出类来，也就是说，动态语言本身支持运行期动态创建类，这和静态语言有非常大的不同，
+要在静态语言运行期创建类，必须构造源代码字符串再调用编译器，或者借助一些工具生成字节码实现，本质上都是动态编译，会非常复杂。
+'''
+
+# 这里省略metaclass
